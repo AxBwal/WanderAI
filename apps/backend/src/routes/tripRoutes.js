9fac70f3f0ca3c3c133eac7f669d1014fea510a2
Akxxxhil <1941012872.e.anshumanbiswal@gmail.com>
2025-03-23 21:38:17 +0530

    new Routes added

--- apps/backend/src/routes/tripRoutes.js
+++ apps/backend/src/routes/tripRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { getTrips, createTrip } from "../controllers/tripController.js";
+import { getTrips, createTrip,getAllTrips,getTripById } from "../controllers/tripController.js";
 import { verifyToken } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.get("/gettrips", verifyToken, getTrips);
 router.post("/createtrip", verifyToken, createTrip);
+router.get("/getAllTrips", verifyToken, getAllTrips);
+router.get("/:id", verifyToken, getTripById);
 
 export default router;
